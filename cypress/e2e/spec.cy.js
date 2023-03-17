3b926db7a1afb7c74b2d05fae9af9d4988598f03
Mark Johnson <mark.johnson@govx.com>
2023-03-17 14:26:35 -0700

    removed unused files

--- cypress/e2e/spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('My First Test', () => {
-  it('Gets, types and asserts', () => {
-    cy.visit('http://localhost:9000');
-
-    cy.contains('About').click();
-    cy.contains('About Page');
-  });
-});
